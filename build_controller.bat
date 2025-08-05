537c31de485cd3e9ae2fce2155cfbca770cc7175
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2025-08-05 14:09:32 +0800

    Delete build_controller.bat

--- build_controller.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building Wallpaper Controller...
-pip install -r requirements.txt
-pip install pyinstaller
-pyinstaller --onefile --windowed --name=wallpaper_controller controller.py
-echo Controller build complete! Check dist/ folder
-pause
